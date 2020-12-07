5a7676eb957cbab772e126b695de7262d813eee9
Debasis Saikia <debasishsaikia136@gmail.com>
2020-12-07 23:01:30 +0530

    'store-action-reducers'

--- redux1/index.js
+++ redux1/index.js
@@ -1,1 +1,35 @@
-console.log('index')
+const redux = require("redux");
+const createStore = redux.createStore;
+
+// string constant
+const BUY_CAKE = "BUY_CAKE";
+
+// define action ..action creator function that returns an actions
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "Initial Redux Action",
+  };
+}
+
+// reducers(previousState,action)=>newState
+// initial state
+const initialState = {
+  numOfCakes: 10,
+};
+
+// reducer function
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+
+const store=createStore(reducer)
