8cfed242392782f5928a9b9fdaf2b0f466ef9827
Debasis Saikia <debasishsaikia136@gmail.com>
2020-12-12 21:52:14 +0530

    'project-setup'

---
+++ leaflet/project.js
@@ -0,0 +1,10 @@
+const map = L.map("map").setView([22.9074872, 79.07306671], 5);
+
+// open street map url
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors-Debasis Saikia';
+
+const tiles = L.tileLayer(tileUrl, { attribution });
+// adding to map
+tiles.addTo(map);
