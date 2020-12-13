be5c002225985dd30090804ea40d2ca2ebe13873
Debasis Saikia <debasishsaikia136@gmail.com>
2020-12-13 22:29:18 +0530

    'fly-to-location'

--- leaflet/project.js
+++ leaflet/project.js
@@ -23,6 +23,11 @@ function generateList() {
     a.href = "#";
     p.innerText = shop.properties.address;
 
+    // adding locate eventListener
+    a.addEventListener('click',()=>{
+        locateStore(shop)
+    })
+
     div.appendChild(a);
     div.appendChild(p);
     li.appendChild(div);
@@ -32,13 +37,41 @@ function generateList() {
 
 generateList();
 
-
 // markups
-function onEachFeature(){
-
+function popUpContent(shop){
+    return `
+        <div>
+        <h4>${shop.properties.name}</h4>
+        <p>${shop.properties.address}</p>
+        <div class="phone_no">
+        <a href="tel:${shop.properties.phone}">${shop.properties.phone}</a>
+        </div>
+        </div>
+    `;
 }
 
-const shopLayer=L.geoJSON(shopList,{
-    onEachFeature:onEachFeature,
-    pointToLayer:function(feature,latlng){}
-})
+function onEachFeature(feature, layer) {
+  layer.bindPopup(popUpContent(feature), {
+    closeButton: false,
+    offset: L.point(0, -8),
+  });
+}
+
+// icons
+const icon = L.icon({
+  iconUrl: "https://cdn.onlinewebfonts.com/svg/img_319799.png",
+  iconSize: [30, 30],
+});
+
+const shopLayer = L.geoJSON(shopList, {
+  onEachFeature: onEachFeature,
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon });
+  },
+});
+shopLayer.addTo(map);
+
+// locate shops
+function locateStore(store){
+map.flyTo([store.geometry.coordinates[1], store.geometry.coordinates[0]]);
+}
